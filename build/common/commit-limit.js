4cb5f22575a3c8e1ebd3beb8c112905158643f43
jialin <jialinmo0718@gmail.com>
2021-03-12 13:41:54 +0800

    feat: 修改提交代码说明格式校验测试

--- build/common/commit-limit.js
+++ build/common/commit-limit.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+console.log('232323');
 const gitPath = path.join(__dirname,'..',process.env.HUSKY_GIT_PARAMS);
 console.log(gitPath);
 process.exit(1);
